70ebed74ef4d5cb46016140d10281d0a7901fd82
Afrith Shariff <afrithshariff20@gmail.com>
2025-09-06 01:54:00 +0000

    censor

--- src/app/Todo/page.js
+++ src/app/Todo/page.js
@@ -344,7 +344,7 @@ function Todos() {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
               <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello Nigga!</h1>
+                <h1 className="mb-5 text-5xl font-bold">Hello Developer!</h1>
                 <p className="mb-5">
                   "No Todo are currently available for you. Create your first
                   Todo with the below botton."
